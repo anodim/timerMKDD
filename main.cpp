d50f330e1635f584ef36f5c80637ec1d81fa0faf
anodim <sauvanetthierry@gmail.com>
2017-08-11 13:19:32 +0200

    ajout de la syncroV et correction bug

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include <fstream>
 
 #define FONT_TITLE_SIZE 40
+#define FONT_PB_SIZE 40
 #define FONT_SPLIT_SIZE 27
+#define BORDURE 15
 
 #define IMG_SIZE 0.90f
 
@@ -19,14 +21,17 @@ Sprite img_s;
 Font arial;
 
 Text txt("Total -- 00:00:000",arial,FONT_TITLE_SIZE);
+Text txt_pb("PB -- 00:00:000",arial,FONT_PB_SIZE);
 
 Text txtCircuit[16];
 Text txtDelta[16];
 Text txtTempsCircuit[16];
-Text txtSomme("+00:00:000",arial,FONT_SPLIT_SIZE);
+Text txtSomme("",arial,FONT_SPLIT_SIZE);//("+00:00:000",arial,FONT_SPLIT_SIZE);
 
 int pb[17];
 int newPb[17];
+int gold[17];
+int newGold[17];
 int win_largeur;
 int win_hauteur;
 RenderWindow *win;
@@ -43,15 +48,22 @@ HANDLE phandle;
 int value;
 int fait[16];
 int somme_pb;
+int current_pb;
 int nbFait;
 
 int minute;
 int sec;
 int milli;
 
+int ligne_posV;
+int img_posV;
+
 char buff[100];
 
+bool isGold;
+
 ifstream fichier("pbMKDD.txt");
+ifstream fichier_gold("goldMKDD.txt");
 /* ============================================== */
 
 /* ================== FONCTION ================== */
@@ -66,30 +78,36 @@ void init()
     // Chargement de l'image
     if(img.loadFromFile("img.png"))
     {
-      //img non charger
+      //img non chargée
     }
+
+    img_posV = 2*FONT_TITLE_SIZE+FONT_PB_SIZE+FONT_SPLIT_SIZE*17;
+
     img_s.setTexture(img);
     img_s.setScale(IMG_SIZE,IMG_SIZE);
-    img_s.setPosition(0,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+15+FONT_SPLIT_SIZE*17); // maj de la pos vertical pour calc de la hauteur de la fenetre
+    img_s.setPosition(0,img_posV); // maj de la pos vertical pour calc de la hauteur de la fenetre
 
-    arial.loadFromFile("../../Font/pixelade.TTF"); //chargemet, de la font
+    arial.loadFromFile("../../Font/pixelade.TTF"); //chargement, de la font
 
-    int imageMax = img.getSize().x/16*IMG_SIZE+30; // calc de la largeur pour le txt
-    int textMax = 22*FONT_SPLIT_SIZE*0.45+30; // calc de la largeur pour l'image
+    int imageMax    = img.getSize().x/16*IMG_SIZE+30; // calc de la largeur pour le txt
+    int textMax     = 22*FONT_SPLIT_SIZE*0.45+30; // calc de la largeur pour l'image
 
     win_largeur = (textMax > imageMax)?textMax:imageMax; // maj de la largeur max
     win_hauteur = img_s.getGlobalBounds().top+img_s.getGlobalBounds().height;
 
-    win = new RenderWindow(VideoMode(win_largeur,win_hauteur+10),"Ano\'s auto-spliter"); // ouverture de la fenetre
+    win = new RenderWindow(VideoMode(win_largeur,win_hauteur+BORDURE),"Timer MKDD"); // ouverture de la fenetre
 
-    //win->setPosition(Vector2i(1920,0));// a virer <----------------------------------------------------------------------------------------
     win->setFramerateLimit(30); // reduit le framerate, augmentation de perf
+    win->setVerticalSyncEnabled(true);
 
     txt.setPosition((win_largeur/2)-(txt.getGlobalBounds().width/2),0); // positionnement du total
 
-    img_s.setPosition((win_largeur/2)-(img_s.getGlobalBounds().width/16)/2,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+15+FONT_SPLIT_SIZE*17); // positionnement definitif de l'image
+    txt_pb.setPosition((win_largeur/2)-(txt_pb.getGlobalBounds().width/2),FONT_TITLE_SIZE+FONT_SPLIT_SIZE); // positionnement du PB
+
+    img_s.setPosition((win_largeur/2)-(img_s.getGlobalBounds().width/16)/2,img_posV); // positionnement definitif de l'image
     img_s.setTextureRect(IntRect(0,0,img_s.getLocalBounds().width/16,img_s.getLocalBounds().height)); // mise en place du masque
 
+    current_pb = 0;
     if(fichier)
     {
         string ligne;
@@ -97,13 +115,14 @@ void init()
         for(int i=0; i<17; i++)
         {
             getline(fichier,ligne);
-            pb[i] = atoi(ligne.c_str());
+            pb[i]       = atoi(ligne.c_str());
+            current_pb  = pb[16];
         }
     }
     else
     {
         ofstream file("log.txt",ios_base::app);
-        file << "fichier non trouver" << endl;
+        file << "fichier non trouvé" << endl;
 
         for(int i=0; i<17; i++)
         {
@@ -113,24 +132,67 @@ void init()
         }
     }
 
-     for(int i=0; i<16; i++)
+    //gold split
+    if(fichier_gold)
     {
+        string ligne;
+
+        for(int i=0; i<17; i++)
+        {
+            getline(fichier_gold,ligne);
+            gold[i]       = atoi(ligne.c_str());
+        }
+    }
+    else
+    {
+        ofstream file("log.txt",ios_base::app);
+        file << "fichier non trouvé" << endl;
+
+        for(int i=0; i<17; i++)
+        {
+            ofstream file2("goldMKDD.txt",ios_base::app);
+            file2 << "5999998" << endl;
+            gold[i] = 5999998;
+        }
+    }
+
+    if(current_pb > 0)
+    {
+        minute      = (current_pb/1000)/60;
+        sec         = (current_pb/1000)%60;
+        milli       = current_pb - (current_pb/1000)*1000;
+
+        char buff_pb[100];
+
+        sprintf(buff_pb,"PB -- %02d:%02d:%03d",minute,sec,milli);
+
+        txt_pb.setString(buff_pb);
+
+    }
+
+
+    for(int i=0; i<16; i++)
+    {
+        ligne_posV = 2*FONT_TITLE_SIZE+FONT_SPLIT_SIZE+BORDURE+FONT_SPLIT_SIZE*i;
+
         txtCircuit[i].setCharacterSize(FONT_SPLIT_SIZE);
         txtCircuit[i].setFont(arial);
-        txtCircuit[i].setPosition(30,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+i*FONT_SPLIT_SIZE+15);
+        txtCircuit[i].setPosition(2*BORDURE,ligne_posV);
+        txtCircuit[i].setString("DDDD");
 
         txtTempsCircuit[i].setCharacterSize(FONT_SPLIT_SIZE);
         txtTempsCircuit[i].setFont(arial);
-        txtTempsCircuit[i].setPosition(30+40,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+i*FONT_SPLIT_SIZE+15);
+        txtTempsCircuit[i].setPosition(2*BORDURE+txtCircuit[i].getGlobalBounds().width,ligne_posV);
         txtTempsCircuit[i].setString(" -- ");
 
         txtDelta[i].setCharacterSize(FONT_SPLIT_SIZE);
         txtDelta[i].setFont(arial);
-        txtDelta[i].setPosition(175,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+i*FONT_SPLIT_SIZE+15);
+        txtDelta[i].setPosition(2*BORDURE+txtCircuit[i].getGlobalBounds().width+txtTempsCircuit[i].getGlobalBounds().width
+                                ,ligne_posV);
         txtDelta[i].setString("");
     }
 
-    txtSomme.setPosition((win_largeur/2)-(txtSomme.getLocalBounds().width/2),FONT_TITLE_SIZE);
+    txtSomme.setPosition((win_largeur/2)-(txtSomme.getGlobalBounds().width/2),FONT_TITLE_SIZE+3);
     txtSomme.setString("");
 
     txtCircuit[0].setString("LC");
@@ -150,33 +212,33 @@ void init()
     txtCircuit[14].setString("BC");
     txtCircuit[15].setString("RR");
 
-    DWORD luigi_circuit = 0x8113F248;
-    DWORD peach_beach = 0x8114B748;
-    DWORD baby_park = 0x8112D848;
-    DWORD dry_dry_desert = 0x811449E8;
-    DWORD mushroom_bridge = 0x8114B3C8;
-    DWORD mario_circuit = 0x81144538;
-    DWORD daisy_cruiser = 0x8114D288;
-    DWORD waluigi_stadium = 0x8114D868;
-    DWORD sherbet_land = 0x8113AD88;
-    DWORD mushroom_city = 0x81145EE8;
-    DWORD yoshi_circuit = 0x81142028;
-    DWORD dk_mountain = 0x8114B4E0;
-    DWORD wario_colosseum = 0x8113DF08;
-    DWORD dino_dino_jungle = 0x8113C9E8;
-    DWORD bowser_castle = 0x81144428;
-    DWORD rainbow_road = 0x810F5F08;
+    DWORD luigi_circuit     = 0x8113F248;
+    DWORD peach_beach       = 0x8114B748;
+    DWORD baby_park         = 0x8112D848;
+    DWORD dry_dry_desert    = 0x811449E8;
+    DWORD mushroom_bridge   = 0x8114B3C8;
+    DWORD mario_circuit     = 0x81144538;
+    DWORD daisy_cruiser     = 0x8114D288;
+    DWORD waluigi_stadium   = 0x8114D868;
+    DWORD sherbet_land      = 0x8113AD88;
+    DWORD mushroom_city     = 0x81145EE8;
+    DWORD yoshi_circuit     = 0x81142028;
+    DWORD dk_mountain       = 0x8114B4E0;
+    DWORD wario_colosseum   = 0x8113DF08;
+    DWORD dino_dino_jungle  = 0x8113C9E8;
+    DWORD bowser_castle     = 0x81144428;
+    DWORD rainbow_road      = 0x810F5F08;
 
-    circuit[0] = luigi_circuit;
-    circuit[1] = peach_beach;
-    circuit[2] = baby_park;
-    circuit[3] = dry_dry_desert;
-    circuit[4] = mushroom_bridge;
-    circuit[5] = mario_circuit;
-    circuit[6] = daisy_cruiser;
-    circuit[7] = waluigi_stadium;
-    circuit[8] = sherbet_land;
-    circuit[9] = mushroom_city;
+    circuit[0]  = luigi_circuit;
+    circuit[1]  = peach_beach;
+    circuit[2]  = baby_park;
+    circuit[3]  = dry_dry_desert;
+    circuit[4]  = mushroom_bridge;
+    circuit[5]  = mario_circuit;
+    circuit[6]  = daisy_cruiser;
+    circuit[7]  = waluigi_stadium;
+    circuit[8]  = sherbet_land;
+    circuit[9]  = mushroom_city;
     circuit[10] = yoshi_circuit;
     circuit[11] = dk_mountain;
     circuit[12] = wario_colosseum;
@@ -188,29 +250,50 @@ void init()
     scoreboard = 0x810933F0;
 
     address = tpsTotalGC;
-    value = 0;
-    hwnd = 0; // handle
+    value   = 0;
+    hwnd    = 0; // handle
 
     for(int i=0; i<16; i++)
         fait[i] = 0;
 
-    somme_pb = 0;
-    nbFait = 0;
+    somme_pb    = 0;
+    nbFait      = 0;
 }
 
 void reset()
 {
+    current_pb = 0;
     for(int i=0; i<16; i++)
     {
         fait[i] = 0;
         txtDelta[i].setString("");
         txtTempsCircuit[i].setString(" -- ");
+        current_pb  += pb[i];
     }
     txt.setString("Total -- 00:00:000");
     txt.setFillColor(Color(255,255,255));
-    somme_pb = 0;
+
+    txt_pb.setString("PB -- 00:00:000");
+    txt_pb.setFillColor(Color(255,255,255));
+
+    if(current_pb > 0)
+    {
+        minute      = (current_pb/1000)/60;
+        sec         = (current_pb/1000)%60;
+        milli       = current_pb - (current_pb/1000)*1000;
+
+        char buff_pb[100];
+
+        sprintf(buff_pb,"PB -- %02d:%02d:%03d",minute,sec,milli);
+
+        txt_pb.setString(buff_pb);
+
+    }
+
+    somme_pb    = 0;
+    nbFait      = 0;
+    img_s.setTextureRect(IntRect(img_s.getLocalBounds().width*nbFait,0,img_s.getLocalBounds().width,img_s.getLocalBounds().height));
     txtSomme.setString("");
-    nbFait = 0;
 }
 
 void readMemory(int &v)
@@ -236,9 +319,9 @@ int main()
                 win->close();
         }
 
-        minute = 0;
-        sec = 0;
-        milli = 0;
+        minute  = 0;
+        sec     = 0;
+        milli   = 0;
 
         // on trouve dolphin lorsqu'il s'ouvre
         if(!hwnd)
@@ -291,16 +374,26 @@ int main()
 
                     img_s.setTextureRect(IntRect(img_s.getLocalBounds().width*nbFait,0,img_s.getLocalBounds().width,img_s.getLocalBounds().height));
 
-                    newPb[i] = value;
-                    minute = (value/1000)/60;
-                    sec = (value/1000)%60;
-                    milli = value - (value/1000)*1000;
+                    newPb[i]    = value;
+                    minute      = (value/1000)/60;
+                    sec         = (value/1000)%60;
+                    milli       = value - (value/1000)*1000;
 
                     sprintf(buff,"%s %02d:%02d:%03d",((string)txtTempsCircuit[i].getString()).c_str(),minute,sec,milli);
                     txtTempsCircuit[i].setString(buff);
 
-                    txtDelta[i].setPosition(txtTempsCircuit[i].getGlobalBounds().left+txtTempsCircuit[i].getGlobalBounds().width+15
-                                            ,FONT_TITLE_SIZE+FONT_SPLIT_SIZE+i*FONT_SPLIT_SIZE+15);
+                    txtDelta[i].setPosition(txtTempsCircuit[i].getGlobalBounds().left+txtTempsCircuit[i].getGlobalBounds().width+BORDURE
+                                            ,2*FONT_TITLE_SIZE+FONT_SPLIT_SIZE+i*FONT_SPLIT_SIZE+BORDURE);
+
+                    if(value < gold[i])
+                    {
+                        newGold[i]  = value;
+                        isGold      = true;
+                    }
+                    else{
+                        newGold[i]  = gold[i];
+                        isGold      = false;
+                    }
 
                     if(pb[i] != 5999998)
                     {
@@ -308,9 +401,10 @@ int main()
 
                         somme_pb += delta;
 
-                        minute = abs((somme_pb/1000)/60);
-                        sec = abs((somme_pb/1000)%60);
-                        milli = abs(somme_pb - (somme_pb/1000)*1000);
+                        minute  = abs((somme_pb/1000)/60);
+                        sec     = abs((somme_pb/1000)%60);
+                        milli   = abs(somme_pb - (somme_pb/1000)*1000);
+
 
                         if(somme_pb > 0)
                         {
@@ -325,17 +419,24 @@ int main()
 
                         txtSomme.setString(buff);
 
-                        minute = abs((delta/1000)/60);
-                        sec = abs((delta/1000)%60);
+                        minute  = abs((delta/1000)/60);
+                        sec     = abs((delta/1000)%60);
 
                         if(delta > 0)
                         {
+
                             txtDelta[i].setFillColor(Color(255,0,0));
                             sprintf(buff,"+%02d:%02d",minute,sec);
                         }
                         else
                         {
-                            txtDelta[i].setFillColor(Color(0,255,0));
+                            if(isGold)
+                            {
+                                txtDelta[i].setFillColor(Color(204,153,0));
+                            }
+                            else{
+                                txtDelta[i].setFillColor(Color(0,255,0));
+                            }
                             sprintf(buff,"-%02d:%02d",minute,sec);
                         }
 
@@ -354,12 +455,14 @@ int main()
             address = tpsTotalGC;
             readMemory(value);
 
-            minute = (value/1000)/60;
-            sec = (value/1000)%60;
-            milli = value - (value/1000)*1000;
+            minute  = (value/1000)/60;
+            sec     = (value/1000)%60;
+            milli   = value - (value/1000)*1000;
 
             if(fait[15] == 1) // fin
             {
+                nbFait = 0;
+                img_s.setTextureRect(IntRect(img_s.getLocalBounds().width*nbFait,0,img_s.getLocalBounds().width,img_s.getLocalBounds().height));
                 int tmp = 0;
                 for(int i=0; i<16; i++)
                 {
@@ -379,6 +482,13 @@ int main()
                             file << newPb[i] << endl;
                         }
                     }
+
+                    ofstream file("goldMKDD.txt",ios::out | ios::trunc);
+                    for(int i=0; i<17; i++)
+                    {
+                        // ecrire nouveau fichier
+                        file << newGold[i] << endl;
+                    }
                 }
             }
 
@@ -391,6 +501,7 @@ int main()
 
         win->clear();
         win->draw(img_s);
+        win->draw(txt_pb);
         win->draw(txt);
         win->draw(txtSomme);
         for(int i=0; i<16; i++)
